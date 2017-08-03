22a581c36df608325d750ca25c123ba9735b6d4b
赵海冰 <zhaohb2991@gmail.com>
2017-08-03 23:31:41 +0800

    index

---
+++ 安卓下拉刷新上拉加载/11/gen/com/example/zhy_pulltorefreash_chenyoca/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.zhy_pulltorefreash_chenyoca;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
